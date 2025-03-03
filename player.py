ec5a2448d680744d08e3428747bfc49b9caa01fa
paako <bootdev@dsts.info>
2025-03-03 11:57:57 +0000

    Fix import of circleshape

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
-import circleshape
 import pygame
 from constants import PLAYER_RADIUS
+from circleshape import CircleShape
 
-class Player(circleshape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
