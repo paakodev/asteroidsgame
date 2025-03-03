7ac33ff01030864e5b22ca1431faef38f8df1492
paako <bootdev@dsts.info>
2025-03-03 09:21:06 +0000

    Restrict to 60 FPS

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     ok, failed = pygame.init()
     print(f"OK: {ok}\t\tFAILED: {failed}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        
+        # Tick the loop
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
