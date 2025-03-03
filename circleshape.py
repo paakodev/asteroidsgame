7f175f2205b0a66a9313a130b6d591c0df841fec
paako <bootdev@dsts.info>
2025-03-03 12:35:54 +0000

    Fix adding to containers

--- circleshape.py
+++ circleshape.py
@@ -3,12 +3,12 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
+        super().__init__()  # No arguments needed here
+        # After init, add self to each container
+        if hasattr(self.__class__, "containers"):
+            for container in self.__class__.containers:
+                container.add(self)
+        
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
